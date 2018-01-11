a0b930ca527a0aaf2220c228e73f2676dfe56387
antonzheng <anton.zheng@gmail.com>
2018-01-10 18:32:24 -0800

    added first app

---
+++ helloios/helloios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloios
+//
+//  Created by Anton Zheng on 1/10/18.
+//  Copyright © 2018 Anton Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
